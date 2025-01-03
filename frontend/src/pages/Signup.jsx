44ba95d3368075c718926b7b1418f25a8bd2707c
Gabrielle  Pile <gabriellepile@Gabrielles-MacBook-Pro.local>
2025-01-02 21:37:16 -0800

    added the changes to the main page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -2,29 +2,34 @@ import  SideBar from '../components/SideBar'
 import SignUpForm  from '../components/SignUpForm'
 import Button from '../components/Button'
 import { Link } from 'react-router-dom';
-function Signup(){
-    return(
-        <div>
-            <div className = "flex">
-                <SideBar className = ""></SideBar> 
-                <div className = "content-center ml-40 pl-6 pt-10">
-                    <div className = "flex flex-col">
-                        <h1 className = "mb-3 text-6xl"> Welcome to RadiX! </h1>
-                        <p className = "text-2xl mb-10">Sign Up below!</p>
-                    </div>
-                    <SignUpForm className = "mb-3"></SignUpForm>
-                   
-                    <div className = " w-full text-center flex mt-8  mb-8">
-                        <hr className = "mt-4 border-1 w-1/2"></hr> <p className = "ml-3 mr-3 text-xl">OR</p>  <hr className = " mt-4 border-1 w-1/2"></hr>
-                    </div>
-                    <Link to  = "/login" ><Button message="Login In"></Button></Link>
 
-                    
+function Signup(){
+    return (
+        <div className="flex h-screen">
+            <SideBar />
+
+            <div className="flex flex-col items-center justify-center w-2/3 px-6">
+                <div className="text-center mb-10">
+                    <h1 className="font-poppins font-bold text-7xl mb-3">Welcome to RadiX!</h1>
+                    <p className="font-poppins text-3xl">Sign Up!</p>
+                </div>
+
+                <div className="w-full max-w-md">
+                    <SignUpForm className="mb-5" />
+                </div>
+
+                <div className="flex items-center w-full max-w-md my-8">
+                    <hr className="flex-grow border-t border-gray-300" />
+                    <p className="mx-4 text-xl">OR</p>
+                    <hr className="flex-grow border-t border-gray-300" />
+                </div>
+                <div className = "w-full  max-w-md">
+                    <Link to  = "/login" ><Button message="Login In"></Button></Link>
                 </div>
                 
-            </div> 
+            </div>
         </div>
-        
     );
+   
 }
 export default Signup;
